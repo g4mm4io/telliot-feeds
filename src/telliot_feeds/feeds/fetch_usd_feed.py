ac31080bb28a8cd4188d1e2f650212f35a27fa01
dsm <diogo@liquidloans.io>
2023-06-30 10:51:50 -0300

    Use CoinGecko as the only source for FETCH/USD

--- src/telliot_feeds/feeds/fetch_usd_feed.py
+++ src/telliot_feeds/feeds/fetch_usd_feed.py
@@ -1,19 +1,9 @@
 """Datafeed for current price of FETCH in USD."""
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
-from telliot_feeds.sources.price.spot.coinbase import CoinbaseSpotPriceSource
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 fetch_usd_median_feed = DataFeed(
     query=SpotPrice(asset="fetch", currency="usd"),
-    source=PriceAggregator(
-        asset="fetch",
-        currency="usd",
-        algorithm="median",
-        sources=[
-            CoinGeckoSpotPriceSource(asset="fetch", currency="usd"),
-            CoinbaseSpotPriceSource(asset="fetch", currency="usd"),
-        ],
-    ),
+    source=CoinGeckoSpotPriceSource(asset="fetch", currency="usd")
 )
