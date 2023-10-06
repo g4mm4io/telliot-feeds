0304dc660aa6522747616ad0d97e421d3e80c208
Jemuel Bryan Vergara <jem@liquidloans.io>
2023-10-06 22:41:14 +0800

    LL-1486: Remove logging

--- podinit.sh
+++ podinit.sh
@@ -7,14 +7,12 @@ telliot config init
 
 pk_path="/mnt/secrets-store/${SECRET_NAME}"
 telliot_pw_path="/mnt/telliot-password/${TELLIOT_PASSWORD}"
-pk_path_contents=$(cat "$pk_path")
-telliot_pw_path_contents=$(cat "$telliot_pw_path")
-export PRIVATE_KEY="$pk_path_contents"
-export ACC_PWD="$telliot_pw_path_contents"
-rm -r /var/log/
+pk_path_contents=$(cat "$pk_path") > /dev/null 2>&1
+telliot_pw_path_contents=$(cat "$telliot_pw_path") > /dev/null 2>&1
+export PRIVATE_KEY="$pk_path_contents" > /dev/null 2>&1
+export ACC_PWD="$telliot_pw_path_contents" > /dev/null 2>&1
 
-echo -e "$ACC_PWD\n$ACC_PWD\n" | telliot account add myacct$ACC_NUMBER $PRIVATE_KEY $NETWORK_ID
+echo -e "$ACC_PWD\n$ACC_PWD\n" | telliot account add myacct$ACC_NUMBER $PRIVATE_KEY $NETWORK_ID > /dev/null 2>&1
 echo "Account myacct$ACC_NUMBER created!"
-rm -r /var/log/
 
-echo -e "yes\n$ACC_PWD\n" | telliot report -a myacct$ACC_NUMBER -qt $QUERY_TAG -s $STAKE_AMOUNT -ncr --fetch-flex -wp $WAIT_PERIOD
+echo -e "yes\n$ACC_PWD\n" | telliot report -a myacct$ACC_NUMBER -qt $QUERY_TAG -s $STAKE_AMOUNT -ncr --fetch-flex -wp $WAIT_PERIOD > /dev/null 2>&1
